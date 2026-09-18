94b8e1f828bb3d402294f37f7311bffaaea2781f
LeBengouz <olivebenjamin38@gmail.com>
2026-09-18 13:43:32 +0200

    [Correction] Avoid duplicates in context + chunk-to-text

--- src/retrieval.py
+++ src/retrieval.py
@@ -18,7 +18,8 @@ def get_local_context(target_id: int, chunks: list[dict], window: int = 1):
 def retrieve_context(target_text, target_chunk, chunks, embedding_model, vector_index):
     """
     Get strcutural AND semantic context
-    semantic context = chunks talking of the same subject
+    Local context = targeted chunk + previous and next chunks
+    Semantic context = chunks talking of the same subject
 
     ToDO : context using markdown titles ? (sections)
     """
@@ -26,10 +27,24 @@ def retrieve_context(target_text, target_chunk, chunks, embedding_model, vector_
     query_embedding = embedding_model.encode([target_text])
 
     scores, indices = vector_index.search(query_embedding, k=5)
-    semantic_context = [chunks[i] for i in indices[0]]
+    local_ids = {chunk["id"] for chunk in local_context}
+
+    semantic_context = [chunks[i] for i in indices[0] if chunks[i]["id"] not in local_ids] # eviter doublons
 
     return {
         "target": target_chunk,
         "local": local_context,
         "semantic": semantic_context,
     }
+
+
+def transform_chunks_to_text(chunks):
+    """
+    chunks : list of dictionaries representing each chunk
+
+    output : 1 string containing all context chunks
+    """
+    texts = []
+    for chunk in chunks:
+        texts.append(chunk["text"])
+    return "\n\n---\n\n".join(texts)
